c796c32b26fd6f647f94ff7f4c2dea0db24589a7
Kelvin-MKL <109009865+Kelvin-MKL@users.noreply.github.com>
2023-05-17 20:16:22 +1000

    file created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="outter">
+       <div className="dot"></div>
+       <div className="dot"></div>
+       <div className="dot"></div>
+       <div className="dot"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
